e3f78a5662b6d8c82d97b5bea0415ee8f10f079f
chmad777 <106173821+chmad777@users.noreply.github.com>
2022-06-19 22:57:40 +0200

    Update ethernet_status.sh

--- config/polybar/scripts/ethernet_status.sh
+++ config/polybar/scripts/ethernet_status.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
- 
-echo "%{F#b1e3ff}$(/usr/sbin/ifconfig wlan0 | grep "inet " | awk '{print $2}')%{u-}"
+
+eth=$(ifconfig eth0 | grep "netmask" | awk '{print $2}')
+wlan=$(ifconfig wlan0 | grep "netmask" | awk '{print $2}')
+
+if [ $eth ]; then
+ echo "%{F#ffffff} {F#b1e3ff}$eth%{u-}" 
+elif [ $wlan ]; then
+ echo "%{F#ffffff} {F#b1e3ff}$wlan%{u-}"
+else
+ echo "Not connected"
+fi
