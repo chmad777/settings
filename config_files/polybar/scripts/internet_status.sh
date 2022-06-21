e48f7fc841ccc8c91e3c53613d73c0653a4bba02
chmad777 <106173821+chmad777@users.noreply.github.com>
2022-06-21 12:34:06 +0200

    ADD INIT 1

---
+++ config_files/polybar/scripts/internet_status.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+eth=$(ifconfig eth0 | grep "netmask" | awk '{print $2}')
+
+#if [ ${ifconfig | grep "wlan") ]; then
+wlan=$(ifconfig wlan0 | grep "netmask" | awk '{print $2}')
+#fi
+
+if [ $eth ]; then
+ echo "%{F#ffffff} %{F#b1e3ff}$eth%{u-}" 
+elif [ $wlan ]; then
+ echo "%{F#ffffff} %{F#b1e3ff}$wlan%{u-}"
+else
+ echo "Not connected"
+fi
