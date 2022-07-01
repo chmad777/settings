c8dd3e54c5cf30d58c8462c35fc0fcb03ba5ac7c
chmad777 <106173821+chmad777@users.noreply.github.com>
2022-07-01 02:06:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ CRED = "\033[1;31m"
 CGREEN = "\033[0;32m"
 CBLUE = "\033[1;34m"
 CYELLOW = "\033[1;33m"
-CPURPLE = "\033[1;35m"
 CWHITE = "\033[1;37m"
 CEND = '\33[0m'
 
@@ -18,14 +17,14 @@ banner = """
 """
 
 def menu():
-    print(CBLUE + banner + CEND)
-    time.sleep(1)
-    print(CYELLOW + "1 -> " + CPURPLE + "Instalar settings")
-    time.sleep(1)
-    print(CYELLOW + "\n2 -> " + CPURPLE + "Salir")
-    time.sleep(1)
+    print(CGREEN + banner + CEND)
+    time.sleep(0.5)
+    print(CYELLOW + "1 -> " + CBLUE + "Instalar settings")
+    time.sleep(0.5)
+    print(CYELLOW + "\n2 -> " + CBLUE + "Salir")
+    time.sleep(0.5)
 
-    option = input("\n-->> ")
+    option = input(CYELLOW + "\n-->> " + CBLUE)
 
     if option == "1":
         install()
@@ -41,9 +40,7 @@ def install():
 	time.sleep(2)
 
 # Instalando Dependencias
-	red()
-	print("\n[+] Instalando requerimientos\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando requerimientos\n" + CEND)
 	time.sleep(1)
 
 	os.system("sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev -y")
@@ -60,37 +57,33 @@ def install():
 	os.system("sudo apt install make-guile -y")
 	os.system("sudo apt autoremove -y && sudo apt update && sudo apt upgrade -y")
 	green()
-	print("\n[!] Requerimientos instalados correctamente\n")
+	print("\n[!] Requerimientos instalados correctamente\n" + CEND)
 	time.sleep(2)
 
 # Instalando bspwm	
-	red()
-	print("\n[+] Instalando bspwm\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando bspwm\n" + CEND)
 	time.sleep(1)
 
 	os.system("cd /opt/settings/repositorios/bspwm && make && sudo make install")
 	os.system("sudo apt install bspwm -y")
 	green()
-	print("\n[!] Bspwm instalado\n")
+	print("\n[!] Bspwm instalado\n" + CEND)
 
 	time.sleep(2)
 
 # Instalando sxhkd	
-	red()
-	print("\n[+] Instalando sxhkd\n")
-	white()
+	
+	print(CRED + "\n[+]" + CYELLOW + " Instalando sxhkd\n" + CEND)
+	
 	time.sleep(1)
 
 	os.system("cd /opt/settings/repositorios/sxhkd && make && sudo make install")
 	green()
-	print("\n[!] sxhkd instalado\n")
+	print("\n[!] sxhkd instalado\n" + CEND)
 	time.sleep(2)
 
 # Copiando archivos de configuracion de bspwm & sxhkd
-	red()
-	print("\n[+] Copiando archivos de configuracion de bspwm y sxhkd\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Copiando archivos de configuracion de bspwm y sxhkd\n" + CEND)
 	time.sleep(1)
 
 	os.system("mkdir ~/.config/bspwm")
@@ -101,151 +94,123 @@ def install():
 	os.system("chmod +x ~/.config/bspwm/bspwmrc")
 	os.system("chmod +x ~/.config/sxhkd/scripts/resize.sh")
 	
-	green()
-	print("\n[!] Archivos de configuracion copiados\n")
+	print(CGREEN + "\n[!]" + CYELLOW + " Archivos de configuracion copiados\n" + CEND)
 	time.sleep(2)
 
 # Instalando kitty
-	red()
-	print("\n[+] Instalando kitty\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando kitty\n" + CEND)
 	time.sleep(1)
 
 	os.system("sudo apt install kitty -y")
 	os.system("mkdir ~/.config/kitty")
 	time.sleep(1)
 	os.system("cp /opt/settings/config_files/kitty/* ~/.config/kitty/")
-	green()
-	print("\n[!] Kitty instalada correctamente\n")
-
+	
+	print(CGREEN + "\n[!]" + CYELLOW + " Kitty instalada correctamente\n" + CEND)
 	time.sleep(2)
+	
 # Instalando polybar
-	red()
-	print("\n[+] Instalando la polybar\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando la polybar\n" + CEND)
 	time.sleep(1)
 	
 	os.system("cd /opt/settings/repositorios && git clone --recursive https://github.com/polybar/polybar")
 	os.system("mkdir /opt/settings/repositorios/polybar/build && cd /opt/settings/repositorios/polybar/build && cmake .. && make -j$(nproc) && sudo make install")
-	green()
-	print("\n[!] Polybar instalada correctamente\n")
-
+	
+	print(CGREEN + "\n[!]" + CYELLOW + " Polybar instalada correctamente\n" + CEND)
 	time.sleep(2)
 
 # Copiando archivos de configuracion de la polybar
-	red()
-	print("\n[+] Copiando los archivos de configuracion de la polybar\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Copiando los archivos de configuracion de la polybar\n" + CEND)
 	time.sleep(1)
 
 	os.system("cp /opt/settings/config_files/polybar ~/.config/ -r && chmod +x ~/.config/polybar/launch.sh && chmod +x ~/.config/polybar/scripts/* && sudo chown root:root /usr/local/share/zsh/site-functions/_bspc")
-	green()
-	print("\n[!] Archivos de configuracion copiados\n")
+	
+	print(CGREEN + "\n[!]" + CYELLOW + " Archivos de configuracion copiados\n" + CEND)
 	time.sleep(2)
 
 # Instalando picom
-	red()
-	print("\n[+] Instalando picom\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando picom\n" + CEND)
 	time.sleep(1)
 
 	os.system("cd /opt/settings/repositorios/picom && git submodule update --init --recursive && meson --buildtype=release . build && ninja -C build && sudo ninja -C build install")
 	os.system("mkdir  ~/.config/picom")
 	os.system("cp /opt/settings/config_files/picom/picom.conf ~/.config/picom")
-	green()
-	print("\n[!]Picom instalado correctamente\n")
-
+	
+	print(CGREEN + "\n[!]" + CYELLOW + " Picom instalado correctamente\n" + CEND)
 	time.sleep(2)
 
 # Instalando Rofi
-	red()
-	print("\n[+] Instalando Rofi\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando Rofi\n" + CEND)
 	time.sleep(1)
 
 	os.system("sudo apt install rofi -y")
-	green()
-	print("\n[!]Rofi instalado correctamente\n")
-
+	
+	print(CGREEN + "\n[!]" + CYELLOW + " Rofi instalado correctamente\n" + CEND)
 	time.sleep(2)
 
 
 # Instalando las fuentes
-	red()
-	print("\n[+] Instalando las fuentes\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando las fuentes\n" + CEND)
 	time.sleep(1)
 
 	os.system("sudo cp /opt/settings/fonts/hack_nerd/* /usr/local/share/fonts")
 	os.system("sudo cp /opt/settings/fonts/polybar/* /usr/share/fonts/truetype")
 	os.system("fc-cache -v")
-	green()
-	print("\n[!]Fuentes instaladas correctamente\n")
 
+	print(CGREEN + "\n[!]" + CYELLOW + " Fuentes instaladas correctamente\n" + CEND)
 	time.sleep(2)
 
 # Instalando Feh
-	red()
-	print("\n[+] Instalando Feh\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando Feh\n" + CEND)
 	time.sleep(1)
 	
 	os.system("sudo apt install feh -y")
 	os.system("mkdir ~/.wallpapers && cd ~/.wallpapers && cp /opt/settings/wallpapers/wallpaper.jpeg .")
 	time.sleep(1)
 	os.system("sudo cp /opt/settings/wallpapers/background /usr/share/desktop-base/kali-theme/login")
-	green()
-	print("\n[!]Feh instaladas correctamente\n")
-
+	
+	print(CGREEN + "\n[!]" + CYELLOW + " Feh instaladas correctamente\n" + CEND)
 	time.sleep(2)
 
 # Instalando Powerlevel10k
-	red()
-	print("\n[+] Instalando Powerlevel10k\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando Powerlevel10k\n" + CEND)
 	time.sleep(1)
 
 	os.system("cp /opt/settings/config_files/powerlevel10k/zshrc ~/.zshrc")
 	os.system("cp /opt/settings/config_files/powerlevel10k/p10k.zsh ~/.p10k.zsh")
 	os.system("sudo cp /opt/settings/config_files/powerlevel10k/zshrc.r /root/.zshrc && sudo chown root:root /root/.zshrc")
 	os.system("sudo cp /opt/settings/config_files/powerlevel10k/p10k.zsh.r /root/.p10k.zsh && sudo chown root:root /root/.p10k.zsh")
-	green()
-	print("\n[!]Powerlevel10k instalado correctamente\n")
-
+	
+	print(CGREEN + "\n[!]" + CYELLOW + " Powerlevel10k instalado correctamente\n" + CEND)
 	time.sleep(2)
 
 # Instalando Bat, Lsd & Ranger
-	red()
-	print("\n[+] Instalando Bat, Lsd & Ranger\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando Bat, Lsd & Ranger\n" + CEND)
 	time.sleep(1)
 
 	os.system("sudo dpkg -i /opt/settings/tools/bat/bat.deb")
 	os.system("sudo dpkg -i /opt/settings/tools/lsd/lsd.deb")
 	os.system("sudo apt install ranger -y")
-	green()
-	print("\n[!]Bat, Lsd & Ranger instalados correctamente\n")
-
+	
+	print(CGREEN + "\n[!]" + CYELLOW + " Bat, Lsd & Ranger instalados correctamente\n" + CEND)
 	time.sleep(2)
 
 # Instalando Fzf
-	red()
-	print("\n[+] Instalando Fzf\n")
-	white()
+	print(CRED + "\n[+]" + CYELLOW + " Instalando Fzf\n" + CEND)
 	time.sleep(1)
 
 	os.system("/opt/settings/repositorios/fzf/./install && sudo /opt/settings/repositorios/fzf/./install")
-	green()
-	print("\n[!]Fzf instalad correctamente\n")
 
+	print(CGREEN + "\n[!]" + CYELLOW + " Fzf instalad correctamente\n" + CEND)
 	time.sleep(2)
 
-	print(CGREEN + "\n[+] TODOOO INSTALADO!!!" + CEND)
+# Fin instalación
+	print(CGREEN + "\n[+] " + CRED + " TODOOO INSTALADO!!!" + CEND)
 
 if __name__ == '__main__':
 	id = os.getuid()
-    
+	os.system("clear")    
 	if id == 0:
 		print(CRED + "\n[!]" + CYELLOW + " No hay que ser root para ejecutar la herramienta" + CEND)
 	else:
-        	menu()
+		menu()
