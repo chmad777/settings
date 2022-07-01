791168d5e2944502c338ac0edde46e90ff0ccdd7
chmad777 <106173821+chmad777@users.noreply.github.com>
2022-07-01 02:36:26 +0200

    Update internet_status.sh

--- config_files/polybar/scripts/internet_status.sh
+++ config_files/polybar/scripts/internet_status.sh
@@ -2,9 +2,9 @@
 
 eth=$(ifconfig eth0 | grep "netmask" | awk '{print $2}')
 
-#if [ ${ifconfig | grep "wlan") ]; then
+if [ ${ifconfig | grep "wlan") ]; then
 wlan=$(ifconfig wlan0 | grep "netmask" | awk '{print $2}')
-#fi
+fi
 
 if [ $eth ]; then
  echo "%{F#ffffff} %{F#b1e3ff}$eth%{u-}" 
