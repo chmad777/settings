be49d64add513955387fe0e265d3c9958ac30aa2
chmad777 <106173821+chmad777@users.noreply.github.com>
2022-07-04 14:12:34 +0200

    Update internet_status.sh

--- config_files/polybar/scripts/internet_status.sh
+++ config_files/polybar/scripts/internet_status.sh
@@ -2,14 +2,14 @@
 
 eth=$(ifconfig eth0 | grep "netmask" | awk '{print $2}')
 
-if [ ${ifconfig | grep "wlan") ]; then
-wlan=$(ifconfig wlan0 | grep "netmask" | awk '{print $2}')
+if [ ${ifconfig wlan0 | grep "netmask" | awk '{print $2}') ]; then
+ wlan=$(ifconfig wlan0 | grep "netmask" | awk '{print $2}')
 fi
 
-if [ $eth ]; then
- echo "%{F#ffffff} %{F#b1e3ff}$eth%{u-}" 
-elif [ $wlan ]; then
+if [ $wlan ]; then
  echo "%{F#ffffff} %{F#b1e3ff}$wlan%{u-}"
+elif [ $eth ]; then
+ echo "%{F#ffffff} %{F#b1e3ff}$eth%{u-}" 
 else
  echo "Not connected"
 fi
